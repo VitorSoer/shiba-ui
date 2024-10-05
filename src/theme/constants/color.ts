6947651350af7a380e602d6536c62b7d2060433a
VitorSoer <contato.soer@gmail.com>
2024-10-05 15:06:56 -0300

    feat: add ocean theme

--- src/theme/constants/color.ts
+++ src/theme/constants/color.ts
@@ -13,11 +13,11 @@ export interface IColor {
 }
 
 export const color: IColor = {
-  background: '#F5F5F5',
-  section: '#FFFFFF',
-  highlight: '#C5C5C5',
-  accent: '#A8A8A8',
-  content: '#121212',
+  background: '#F5F5FB',
+  section: '#FBFBFB',
+  highlight: '#D5D5D5',
+  accent: '#A5A5A5',
+  content: '#151515',
   primary: '#7F2BFF',
   secondary: '#486DF7',
   alternative: '#EF5975',
@@ -25,7 +25,6 @@ export const color: IColor = {
   warning: '#E0B02C',
   error: '#C33336',
 };
-
 
 export const darkThemeColors: Partial<IColor> = {
   background: '#121212',
@@ -35,7 +34,13 @@ export const darkThemeColors: Partial<IColor> = {
   content: '#FFFFFF',
 };
 
+export const oceanThemeColors: Partial<IColor> = {
+  background: '#151B30',
+  section: '#2F3652',
+  highlight: '#B5B5B5',
+  accent: '#2B2B35',
+  content: '#F7F7F7',
+};
 
 export type ColorKeys = keyof typeof color;
 export type ColorType = ColorKeys | string;
-
--- src/theme/providers/index.tsx
+++ src/theme/providers/index.tsx
@@ -5,20 +5,23 @@ import theme from '../../theme';
 import { IStyledProvider } from './types';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from '../../theme/config/globalStyles';
-import { color, darkThemeColors } from '../constants/color';
+import { color, darkThemeColors, oceanThemeColors } from '../constants/color';
 
 const getThemeColors = (themeVariant: string) => {
   if (themeVariant == 'light') return { ...color };
   if (themeVariant === 'dark') return { ...color, ...darkThemeColors };
+  if (themeVariant === 'ocean') return { ...color, ...oceanThemeColors };
 };
 
 export const StyledThemeProvider: React.FC<IStyledProvider> = ({
   children,
   customTheme = {},
   customFonts = '',
-  themeVariant = 'light',
+  themeVariant,
 }) => {
-  const baseColors = getThemeColors(themeVariant);
+  const finalThemeVariant = themeVariant || 'light';
+
+  const baseColors = getThemeColors(finalThemeVariant);
   const mergedTheme = { ...theme, color: baseColors, ...customTheme };
 
   return (
