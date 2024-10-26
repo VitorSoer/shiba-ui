9d68376b682e6b5fb2896c0e7393a9f0f4c4ca51
VitorSoer <contato.soer@gmail.com>
2024-10-26 13:44:21 -0300

    📝chore: add nanoid mock test

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ const config: Config.InitialOptions = {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/tests/config/fileMock.ts',
+    '^nanoid.*$': '<rootDir>/src/tests/config/mockNanoid.ts',
   },
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
