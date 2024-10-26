5a886d328da4ae650f6c3b47c7499cfda6bd879e
VitorSoer <contato.soer@gmail.com>
2024-10-26 17:12:19 -0300

    📝 chore: remove unnecessary dependencies

--- .storybook/config/main.ts
+++ .storybook/config/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
   ],
   addons: [
     '@storybook/addon-a11y',
-    '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
